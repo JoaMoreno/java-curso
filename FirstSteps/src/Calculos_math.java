850363816361df90b0d1d8ab742273dc1266a8f4
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-02-07 20:47:22 -0300

    first commit

---
+++ FirstSteps/src/Calculos_math.java
@@ -0,0 +1,15 @@
+
+public class Calculos_math {
+
+	public static void main(String[] args) {
+		/*
+		double a = 9;
+		double raiz = Math.sqrt(a);
+		
+		System.out.println(Math.round(raiz));
+		*/
+		
+		
+	}
+
+}
