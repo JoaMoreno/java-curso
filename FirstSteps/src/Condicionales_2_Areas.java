7fd403cc94d1e73a1e6159c704823cfb98027ecd
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-02-08 20:06:08 -0300

    Condicioales 2 Áreas

--- FirstSteps/src/Condicionales_2_Areas.java
+++ FirstSteps/src/Condicionales_2_Areas.java
@@ -5,6 +5,7 @@ public class Condicionales_2_Areas {
 
 	public static void main(String[] args) {
 
+		@SuppressWarnings("resource")
 		Scanner entrada = new Scanner(System.in);
 
 		System.out.println(
@@ -17,13 +18,34 @@ public class Condicionales_2_Areas {
 			
 			int lado = Integer.parseInt(JOptionPane.showInputDialog("Introduce el lado: "));
 			
-			
-			
-			System.out.println();
-			
+			System.out.println("El área del cuadrado es "+ Math.pow(lado, 2));
 			break;
+		
+		case 2:
+			
+			int base = Integer.parseInt(JOptionPane.showInputDialog("Introduce la base"));
+			int altura = Integer.parseInt(JOptionPane.showInputDialog("Introduce la la altura"));
 
+			System.out.println("El área del rectángulo es " + (base * altura));
+			break;
+		
+		case 3:
+			
+			base = Integer.parseInt(JOptionPane.showInputDialog("Introduce la base"));
+			altura = Integer.parseInt(JOptionPane.showInputDialog("Introduce la la altura"));
+			
+			System.out.println("El área del triángulo es "+ ((base * altura)/2));
+			break;
+		case 4:
+			
+			int radio = Integer.parseInt(JOptionPane.showInputDialog("Introduce el Radio"));
+			
+			System.out.printf("%1.2f",(Math.PI * (Math.pow(radio, 2))));
+			break;
+			
 		default:
+			
+			System.out.println("La opcion no es correcta");
 			break;
 		}
 		
