8a88cac6cbb09b02fb863a60657d7e8e1a749e33
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-02-09 23:11:05 -0300

    POO

---
+++ FirstSteps/src/poo/Car.java
@@ -0,0 +1,21 @@
+package poo;
+
+public class Car {
+	
+	private int ruedas;
+	private int largo;
+	private int ancho;
+	private int motor;
+	private int peso;
+	
+	public Car() {
+		
+		ruedas = 4;
+		largo = 2000;
+		ancho = 300;
+		motor = 1600;
+		peso = 500;
+		
+	}
+
+}
---
+++ FirstSteps/src/poo/Car_Description.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Car_Description {
+	
+	public static void main(String args[]) {
+		
+		// Instanciando Clase
+		Car Renault = new Car();
+		
+		//System.out.println(Renault.ruedas);
+		
+	}
+}
